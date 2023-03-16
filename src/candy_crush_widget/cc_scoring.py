5b4fb3bee29eb064dddbd66fd9871b2c4ebb4efe
Matthew <misayan@gmail.com>
2023-03-16 14:39:01 -0700

    Basic scoring

---
+++ src/candy_crush_widget/cc_scoring.py
@@ -0,0 +1,9 @@
+from tilematch_tools.model import Scoring
+
+class CCScore(Scoring):
+    def __init__(self):
+            super().__init__()
+
+    def award_for_match(self, match):
+        self._points += len(match.matching_tiles)
+
