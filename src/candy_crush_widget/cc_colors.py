f1e381ed944fccfc6bdc23db4603a2c8a7a83770
Matthew <misayan@gmail.com>
2023-03-16 14:46:33 -0700

    Color update

--- src/candy_crush_widget/cc_colors.py
+++ src/candy_crush_widget/cc_colors.py
@@ -2,9 +2,9 @@ from enum import StrEnum
 
 
 class CCTileColor(StrEnum):
-    GREEN = "#7fcfa4"	
-    BLUE = "#6c90ed"	
-    RED = "#e14a4a"	
-    ORANGE = "#f48945"	
-    YELLOW = "#f1d367"	
-    
+    GREEN = "#5cb85c"	
+    BLUE = "#428bca"	
+    RED = "#d9534f"	
+    ORANGE = "#ffb21b"	
+    PURPLE = "#7289da"	
+
