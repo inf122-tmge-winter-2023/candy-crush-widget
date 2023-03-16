167fa2e8d88b6b96e462ae1923ee7ae77b3d575e
Matthew <misayan@gmail.com>
2023-03-16 14:38:31 -0700

    Some non-accented colors

---
+++ src/candy_crush_widget/cc_colors.py
@@ -0,0 +1,10 @@
+from enum import StrEnum
+
+
+class CCTileColor(StrEnum):
+    GREEN = "#7fcfa4"	
+    BLUE = "#6c90ed"	
+    RED = "#e14a4a"	
+    ORANGE = "#f48945"	
+    YELLOW = "#f1d367"	
+    
