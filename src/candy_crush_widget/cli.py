78f134069d64af5029672b01f3bcae406214cc5e
Matthew <misayan@gmail.com>
2023-03-16 14:46:45 -0700

    Basic window up

--- src/candy_crush_widget/cli.py
+++ src/candy_crush_widget/cli.py
@@ -4,10 +4,33 @@
     :module_author: Nathan Mendoza (nathancm@uci.edu)
 """
 
+import logging
 import click
-import tilematch_tools
+
+from tilematch_tools import View, GameEngine, BoardFactory, \
+    GameState, LOGGER
+
+from candy_crush_widget.initial_tiles import initial_tiles
+from candy_crush_widget.cc_game_loop import CCGameLoop
+from candy_crush_widget.cc_scoring import CCScore
+from candy_crush_widget.cc_game_board import CCGameBoard
+
+LOGGER.setLevel(logging.ERROR)
+
 
 @click.command()
 def candy_crush():
     """Entry point to candy-crush"""
-    click.echo('Hello World!')
+    cc_init()
+
+
+def cc_init():
+    board = BoardFactory.create_board_with_tiles(CCGameBoard, 10, 10, initial_tiles(10,10))
+    score = CCScore()
+    state = GameState(board, score)
+    view = View(state)
+
+    loop = CCGameLoop(state, view)
+
+    engine = GameEngine([loop])
+    engine.run()
