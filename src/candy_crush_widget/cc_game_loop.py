b8d5eeb0542d81b133e64609d35a653d32d44407
Matthew <misayan@gmail.com>
2023-03-17 00:41:48 -0700

    Find matches

--- src/candy_crush_widget/cc_game_loop.py
+++ src/candy_crush_widget/cc_game_loop.py
@@ -1,11 +1,17 @@
+from collections.abc import Iterable
 from tilematch_tools.core import GameLoop 
 from tilematch_tools import MatchCondition
 class CCGameLoop(GameLoop):
-    def clear_matches(self, matches_found: [MatchCondition.MatchFound]) -> None:
+    def clear_matches(self, matches_found: Iterable[MatchCondition.MatchFound]) -> None:
         return super().clear_matches(matches_found)
     
-    def find_matches(self, match_rules: [MatchCondition]) -> [MatchCondition.MatchFound]:
-        return super().find_matches(match_rules)
+    def find_matches(self, match_rules: Iterable[MatchCondition]) -> Iterable[MatchCondition.MatchFound]:
+        matches_found = []
+        for match_rule in match_rules:
+            match_found = match_rule.check_match(self.state.board, 1, 1)
+            if match_found is not None:
+                matches_found.append(match_found)
+        return matches_found
     
     def gameover(self) -> bool:
         return super().gameover()
