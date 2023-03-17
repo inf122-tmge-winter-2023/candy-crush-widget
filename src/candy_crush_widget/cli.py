6e0b90693e9551dd8a201710c61baebe1e1f392c
Matthew <misayan@gmail.com>
2023-03-17 00:42:37 -0700

    adding matches

--- src/candy_crush_widget/cli.py
+++ src/candy_crush_widget/cli.py
@@ -9,6 +9,7 @@ import click
 
 from tilematch_tools import View, GameEngine, BoardFactory, \
     GameState, LOGGER
+from candy_crush_widget.cc_match_rules import HorizontalMatch, VerticalMatch
 
 from candy_crush_widget.initial_tiles import initial_tiles
 from candy_crush_widget.cc_game_loop import CCGameLoop
@@ -37,7 +38,9 @@ def cc_init(rows, cols):
     state = GameState(board, score)
     view = View(state)
 
+    state.add_match_condition(VerticalMatch(1))
+    state.add_match_condition(HorizontalMatch(1))
     loop = CCGameLoop(state, view)
-
+    
     engine = GameEngine([loop])
     engine.run()
