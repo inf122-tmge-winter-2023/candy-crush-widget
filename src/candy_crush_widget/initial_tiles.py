bcdeaffa0347ff1c6f5d818247198a525291eabb
Matthew <misayan@gmail.com>
2023-03-17 00:42:52 -0700

    Constructing correct tile

--- src/candy_crush_widget/initial_tiles.py
+++ src/candy_crush_widget/initial_tiles.py
@@ -2,6 +2,7 @@ import random
 from tilematch_tools import TileBuilder
 
 from candy_crush_widget.cc_colors import CCTileColor
+from candy_crush_widget.cc_tile import CCTile
 
 builder = TileBuilder()
 def initial_tiles(num_rows, num_cols):
@@ -9,6 +10,6 @@ def initial_tiles(num_rows, num_cols):
     colors = list(CCTileColor)
     for x in range(1,num_rows + 1):
         for y in range(1,num_cols + 1):
-            tiles.append(builder.add_position(x,y).add_color(random.choice(colors)).construct())
+            tiles.append(builder.add_position(x,y).add_color(random.choice(colors)).construct(CCTile))
 
     return tiles
