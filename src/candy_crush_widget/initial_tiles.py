baf8c6ebf304f25de452f5e4f7447e27210dac31
Matthew <misayan@gmail.com>
2023-03-18 13:50:26 -0700

    Check for non-matching tiles

--- src/candy_crush_widget/initial_tiles.py
+++ src/candy_crush_widget/initial_tiles.py
@@ -1,6 +1,6 @@
 import random
-from tilematch_tools import TileBuilder
-
+from tilematch_tools import TileBuilder, MatchCondition
+from collections.abc import Iterable
 from candy_crush_widget.cc_colors import CCTileColor
 from candy_crush_widget.cc_tile import CCTile
 
@@ -13,3 +13,10 @@ def initial_tiles(num_rows, num_cols):
             tiles.append(builder.add_position(x,y).add_color(random.choice(colors)).construct(CCTile))
 
     return tiles
+
+
+def contains_matches(board, match_rules: Iterable[MatchCondition]):
+    for match_rule in match_rules:
+        if match_rule.check_match(board, 1, 1) is not None:
+            return True
+    return False
