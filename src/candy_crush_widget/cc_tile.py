edd1cffc03d41a8021124e8ce405f4fe7e82d67d
Matthew <misayan@gmail.com>
2023-03-18 23:03:59 -0700

    Make CCtiles movable

--- src/candy_crush_widget/cc_tile.py
+++ src/candy_crush_widget/cc_tile.py
@@ -1,5 +1,9 @@
 from tilematch_tools import Tile
 
 class CCTile(Tile):
+    def __init__(self, **properties):
+        super().__init__(**properties)
+        self._movable = True
+
     def __hash__(self):
         return hash(self.position.x + self.position.y * 89)
