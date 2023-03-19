647fe1a01b0c5fe8c75795217cfe27d76dbac7fb
Matthew <misayan@gmail.com>
2023-03-18 22:52:37 -0700

    Added candy crush logger

--- src/candy_crush_widget/__init__.py
+++ src/candy_crush_widget/__init__.py
@@ -10,3 +10,19 @@ from .cc_game_loop import CCGameLoop
 from .cc_match_rules import *
 from .cc_scoring import Scoring
 from .cc_tile import CCTile
+from .cc_movement_rules import AbsoluteDescent
+
+from tilematch_tools import LOGGER as TMT_LOGGER
+import logging
+
+TMT_LOGGER.setLevel(logging.CRITICAL)
+
+CC_LOGGER = logging.getLogger(__name__)
+LOG_HANDLER = logging.StreamHandler()
+LOG_FORMAT = logging.Formatter('[%(asctime)s|%(name)s|%(levelname)s] - %(message)s')
+
+CC_LOGGER.setLevel(logging.CRITICAL)
+LOG_HANDLER.setLevel(logging.CRITICAL)
+
+LOG_HANDLER.setFormatter(LOG_FORMAT)
+CC_LOGGER.addHandler(LOG_HANDLER)
