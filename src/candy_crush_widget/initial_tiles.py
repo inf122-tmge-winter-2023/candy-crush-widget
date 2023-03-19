69772912e890ab7849b36d0715ada6a19fce1d7f
Matthew <misayan@gmail.com>
2023-03-19 14:04:58 -0700

    Adding counter to init tiles

--- src/candy_crush_widget/initial_tiles.py
+++ src/candy_crush_widget/initial_tiles.py
@@ -1,8 +1,12 @@
+import logging
 import random
 from tilematch_tools import TileBuilder, MatchCondition
 from collections.abc import Iterable
 from candy_crush_widget.cc_colors import CCTileColor
 from candy_crush_widget.cc_tile import CCTile
+
+LOGGER = logging.getLogger(__name__)
+COUNT = 0
 
 builder = TileBuilder()
 def initial_tiles(num_rows, num_cols):
@@ -11,7 +15,9 @@ def initial_tiles(num_rows, num_cols):
     for x in range(1,num_rows + 1):
         for y in range(1,num_cols + 1):
             tiles.append(builder.add_position(x,y).add_color(random.choice(colors)).construct(CCTile))
-
+    global COUNT
+    COUNT = COUNT + 1
+    LOGGER.info(f"Generated {COUNT} sets of tiles")
     return tiles
 
 
