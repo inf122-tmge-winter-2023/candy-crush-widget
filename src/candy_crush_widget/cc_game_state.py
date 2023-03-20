28e4e74e1fb1407c085eb2d20f4a901118729aa4
Matthew <misayan@gmail.com>
2023-03-19 23:21:29 -0700

    Move method

--- src/candy_crush_widget/cc_game_state.py
+++ src/candy_crush_widget/cc_game_state.py
@@ -1,8 +1,8 @@
-from tilematch_tools import GameState, TileColor
+from tilematch_tools import GameState, TileColor, NullTile
 
 from .cc_game_board import CCGameBoard
 from .cc_scoring    import CCScore
-from tilematch_tools.model.match import MatchCondition
+from candy_crush_widget.cc_movement_rules import AbsoluteDescent
 
 class CCGameState(GameState):
     def __init__(self, board: CCGameBoard, score: CCScore):
@@ -20,4 +20,10 @@ class CCGameState(GameState):
             self.selected_tile.border = TileColor.GRAY
             self.selected_tile = None
         
-        
+    def collapse_all(self):
+        for x in range(1, self.board.num_cols + 1):
+            for y in range(1, self.board.num_rows + 1):
+                if(not isinstance(self.board.tile_at(x,y), NullTile)):
+                    collapse = AbsoluteDescent()
+                    collapse.move(self.board, self.board.tile_at(x,y))
+                    
