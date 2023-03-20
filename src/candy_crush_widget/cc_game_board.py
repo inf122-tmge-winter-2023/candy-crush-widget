a4b4818081cc8fe6577a699231cf3ad467c9ffa0
Matthew <misayan@gmail.com>
2023-03-19 23:19:56 -0700

    Moved to game_state

--- src/candy_crush_widget/cc_game_board.py
+++ src/candy_crush_widget/cc_game_board.py
@@ -1,16 +1,8 @@
 from tilematch_tools import GameBoard, NullTile
 
-from candy_crush_widget.cc_movement_rules import AbsoluteDescent
 
 class CCGameBoard(GameBoard):
     def __init__(self, width, height):
         super().__init__(width, height)
 
     # TODO Move somewhere else
-    def collapse_all(self):
-        for x in range(1, self.num_cols + 1):
-            for y in range(1, self.num_rows + 1):
-                if(not isinstance(self.tile_at(x,y), NullTile)):
-                    collapse = AbsoluteDescent()
-                    collapse.move(self, self.tile_at(x,y))
-                    
